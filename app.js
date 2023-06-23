47cd2c00a8345f900f311385c1c2cd5332f02c43
christaiwo <christopherijagbemi@gmail.com>
2023-06-23 16:21:04 +0100

    ***

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import { PrismaClient } from "@prisma/client";
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 import articleRoute from "./routes/articleRoute.js";
 import userRoute from "./routes/userRoute.js";
 
